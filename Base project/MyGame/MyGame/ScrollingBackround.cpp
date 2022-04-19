24f1db855ca173181e6716df90c7f26c469d66aa
JaydenT040 <90404856+JaydenT040@users.noreply.github.com>
2022-04-19 09:37:27 -0700

    update

--- Base project/MyGame/MyGame/ScrollingBackround.cpp
+++ Base project/MyGame/MyGame/ScrollingBackround.cpp
@@ -9,20 +9,17 @@ ScrollingBackround::ScrollingBackround() {
 	sprite2_.setTexture(GAME.getTexture("Resources/background.png"));
 	sprite_.setPosition(pos);
 	sf::Vector2f pos2 = sf::Vector2f(sprite_.getGlobalBounds().width, pos.y);
-	//pos2.x += sprite_.getGlobalBounds().width;
 	sprite2_.setPosition(pos2);
 }
 
 void ScrollingBackround::draw() {
 	GAME.getRenderWindow().draw(sprite_);
 	GAME.getRenderWindow().draw(sprite2_);
-
 }
 
 void ScrollingBackround::update(sf::Time& elapsed) {
 	int msElapsed = elapsed.asMilliseconds();
 	sf::Vector2f pos = sprite_.getPosition();
-	std::cout << "s1: " << pos.x;
 	if (pos.x <= sprite_.getGlobalBounds().width * -1) { 
 		pos.x = sprite_.getGlobalBounds().width;
 		sprite_.setPosition(pos);
@@ -32,7 +29,6 @@ void ScrollingBackround::update(sf::Time& elapsed) {
 	}
 	
 	sf::Vector2f pos2 = sprite2_.getPosition();
-	std::cout << " s2: " << pos2.x << '\n';
 
 
 	if (pos2.x <= sprite2_.getGlobalBounds().width * -1) {
